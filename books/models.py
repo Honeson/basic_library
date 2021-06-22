c7727e343d147a87752f2aa2b7cd540693a7a8e6
Nwovu Sunday <honesty1ofa1i@gmail.com>
2021-06-23 00:16:22 +0100

    First commit

---
+++ books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=225)
+    subtitle = models.CharField(max_length=225)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+
+
+    def __str__(self):
+        return self.title
